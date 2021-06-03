c1a69998725b4daef87ccfba1042d294e9e419a5
alexeyosipov88 <aoosipov88@gmail.com>
2021-06-03 16:43:40 +0000

    fetcher added

---
+++ fetcher.js
@@ -0,0 +1,22 @@
+const args = process.argv.slice(2);
+const request = require('request');
+const fs = require('fs');
+const fetcher = (url, callback) => {
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
+      return;
+    } else {
+      callback(body);
+    }
+  });
+};
+fetcher(args[0], (body) =>{
+    fs.writeFile(args[1], body, err => {
+      if (err) {
+        console.error(err)
+        return;
+      } else console.log(`Donwloaded and saved ${fs.statSync(args[1]).size} bytes in ${args[1]} bytes`)
+    })
+});
+
